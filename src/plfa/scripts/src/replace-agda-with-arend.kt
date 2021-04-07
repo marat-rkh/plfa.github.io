8e46312ddef4fab5077395ca3701dbec54c7f90e
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2021-04-07 16:36:20 +0300

    Add kotlin scripts properly

---
+++ src/plfa/scripts/src/replace-agda-with-arend.kt
@@ -0,0 +1,8 @@
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.streams.*
+
+fun main() {
+    val markdownFiles = Files.list(Path.of("..", "part1")).filter { it.fileName.endsWith(".lagda.md") }.toList()
+    val arendFiles = Files.list(Path.of("..", "part1-arend/src")).filter { it.fileName.endsWith(".ard") }.toList()
+}
